f2a4d6a0229f311f30dfae201a4dbb0cc5e97ebe
Hans Guntersson <daniel.harlos@gmail.com>
2018-09-20 17:56:18 +0100

    Initial Commit

---
+++ Flipping HellTests/Flipping_HellTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flipping_HellTests.swift
+//  Flipping HellTests
+//
+//  Created by Daniel Harlos on 20/09/2018.
+//  Copyright © 2018 Hans Guntersson Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import Flipping_Hell
+
+class Flipping_HellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
