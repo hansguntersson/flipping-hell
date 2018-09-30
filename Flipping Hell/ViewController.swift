c242b264d591af5db625a1e0f751fea5e19aaa10
Hans Guntersson <daniel.harlos@gmail.com>
2018-09-30 18:01:41 +0100

    Initialisation of the game

--- Flipping Hell/ViewController.swift
+++ Flipping Hell/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func clickButton(_ sender: UIButton) {
+        let button = sender
+        let AnimTime = 0.35
+        let xVal = button.center.x
+        let yVal = button.center.y
+        
+        UIView.animate(withDuration: AnimTime, animations:  {
+            button.frame.size = CGSize(width: 0, height: 50.0)
+            button.layer.cornerRadius = 2
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.8, blue: 0, alpha: 1)
+            button.center = CGPoint(x: xVal, y: yVal)
+        })
+        
+        UIView.animate(withDuration: AnimTime, delay: AnimTime, animations: {
+            button.frame.size = CGSize(width: 50, height: 50.0)
+            button.layer.cornerRadius = 25
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.231372549, blue: 0.1882352941, alpha: 1)
+            button.center = CGPoint(x: xVal, y: yVal)
+        })
+        
+    }
+    
 }
 
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
