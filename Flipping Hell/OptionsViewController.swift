dc724cfd8d3e2b5eff8bbc866671958229f0f332
Hans Guntersson <daniel.harlos@gmail.com>
2019-01-03 15:34:46 +0000

    Added back functionality to options and about screens

---
+++ Flipping Hell/OptionsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OptionsViewController.swift
+//  Flipping Hell
+//
+//  Created by Daniel Harlos on 03/01/2019.
+//  Copyright © 2019 Hans Guntersson Ltd. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backToMainScreen(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
