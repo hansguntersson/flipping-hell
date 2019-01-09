9b76d6191135aee27364127c2901e39089ef34f0
Hans Guntersson <daniel.harlos@gmail.com>
2019-01-09 13:06:25 +0000

    Added class for Stage collection view cell

---
+++ Flipping Hell/StageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StageCollectionViewCell.swift
+//  Flipping Hell
+//
+//  Created by Daniel Harlos on 09/01/2019.
+//  Copyright © 2019 Hans Guntersson Ltd. All rights reserved.
+//
+
+import UIKit
+
+class StageCollectionViewCell: UICollectionViewCell {
+    
+}
