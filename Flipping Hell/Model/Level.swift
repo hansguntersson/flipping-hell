7e7cefb3d4fca3be5ba10b4bb209567f9a8e9781
Hans Guntersson <daniel.harlos@gmail.com>
2019-01-23 19:04:24 +0000

    UI work and starting to break out data

--- Flipping Hell/Model/Level.swift
+++ Flipping Hell/Model/Level.swift
@@ -9,25 +9,37 @@
 import Foundation
 
 class Level {
-    let sequence: Array<Int> // The array sequence for the level
+    
+    // Core data
+    var sequenceID: Int = 0 // The unique sequence ID
+    var sequence: Array<Int> // The array sequence for the level
     var goalFlips: Int // The goal flips for the level
     var currentFlips: Int = 0 // The current flip count
-    let currentSequence: Array<Int> // The array sequence for the level
+    let levelIndex: Int // Which index the level is at
+
+    static var levelsIndices = -1 // The class index for the levels
     
-    var isUnlocked = false // Whether the level is unlocked
-    var isComplete = false // Whether the level has been completed
+    // Move to game
     var attempts = 0 // Hpw many attempts were made
     var minFlips: Int? = nil // The minimum number of flips the level has been completed in
     var minMoves: Array<Int> = [0] // The minimum moves the level has been completed in
+    var isComplete = false // Whether the level has been completed
+    var isUnlocked = false // Whether the level is unlocked
+
+    
+    // Passed to Win
     var LevelScore = 0
     var levelstars = "☆ ☆ ☆" // The star sequence for the level
+
+
+ 
+
+    // FUNCT
     
-    let levelIndex: Int // Which index the level is at
-    static var levelsIndices = -1 // The class index for the levels
     
     init(sequence: Array<Int>, goalFlips: Int) {
         self.sequence = sequence
-        self.currentSequence = sequence
+        self.sequence = sequence
         self.goalFlips = goalFlips
         levelIndex = Level.getLevelNumber()
     }
